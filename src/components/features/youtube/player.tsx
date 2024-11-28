b019b1c7ddef97bdab37bf4a825cc77c90d0cd36
Chris Stanlake <chris@codetopixels.com>
2024-11-28 15:55:12 +0000

    add autoplay for youtube component

--- src/components/features/youtube/player.tsx
+++ src/components/features/youtube/player.tsx
@@ -41,7 +41,7 @@ export const YouTubePlayer = ({ videoId }) => {
         </button>
       ) : (
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
