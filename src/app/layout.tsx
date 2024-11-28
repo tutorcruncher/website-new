5b06797ab910b0f8b301b2bf43159bc052627365
Chris Stanlake <chris@codetopixels.com>
2024-11-28 17:07:44 +0000

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import AnimateObserver from "@/components/AnimateObserver";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default async function RootLayout({ children }) {
         <link rel="stylesheet" href="https://use.typekit.net/mnd5til.css" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      {/* <GoogleTagManager gtmId="GTM-K7CNPB" /> */}
+      <GoogleTagManager gtmId="GTM-M6QLN6V3" />
       <body>
         <Header />
         <main>{children}</main>
