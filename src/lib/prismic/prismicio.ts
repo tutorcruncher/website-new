002c0f4686e854a7fd0140eb900538a5aa6af0c6
Chris Stanlake <chris@codetopixels.com>
2025-01-09 09:55:27 +0000

    remove cache on build and revalidate data fetched from prismic

--- src/lib/prismic/prismicio.ts
+++ src/lib/prismic/prismicio.ts
@@ -10,10 +10,10 @@ let client: prismic.Client<AllDocumentTypes>;
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   if (!client) {
     client = prismic.createClient(repositoryName, {
-      fetchOptions:
-        process.env.NODE_ENV === "production"
-          ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-          : { next: { revalidate: 50 } },
+      fetchOptions: {
+        next: { revalidate: 0, tags: ["prismic"] },
+        cache: "force-cache",
+      },
       ...config,
     });
 
