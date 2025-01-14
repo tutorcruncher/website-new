60e7886e89dea6e5efe59b9194747fd0b77b31ff
DanJezeph15 <danieljezeph@hotmail.co.uk>
2025-01-14 12:18:28 +0000

    Change redirects to 301

--- redirects/integrations.js
+++ redirects/integrations.js
@@ -3,61 +3,73 @@ const integrationsRedirects = [
     source: "/getting-paid/stripe",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/getting-paid/quickbooks",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/getting-paid/xero",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/getting-paid/gocardless",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/getting-paid/transferwise",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/classin",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/pencilspaces",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/zoom",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/merithub",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/groupworld",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/lessonspace",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
   {
     source: "/tutoring-online/bitpaper",
     destination: "/integrations",
     permanent: true,
+    statusCode: 301,
   },
 ];
 
